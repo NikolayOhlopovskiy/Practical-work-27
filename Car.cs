c0893a72486a2927186ce393ac534e04463a9039
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2025-03-06 23:10:27 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public Car()
+        { }
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public override string ToString()
+        {
+            return $"Марка: {Make}\nМодель: {Model}\nГод: {Year}";
+        }
+    }
+}
